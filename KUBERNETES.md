c1bfce47e400420d2557ecf3464842c9afd8e312
Teemu Koivisto <tkoivisto456@gmail.com>
2018-08-29 13:57:38 +0300

    Add working postgres setup with k8s & update seed-db.sh

--- KUBERNETES.md
+++ KUBERNETES.md
@@ -4,6 +4,6 @@
 When using local kubernetes such as minikube you can't expose services through LoadBalancer as in a cloud provider. So you have to use NodePorts instead. https://stackoverflow.com/questions/44110876/kubernetes-service-external-ip-pending
 
 
-You can create secrets with `kubectl create secret `
+You can create generic secrets with `kubectl create secret generic my-postgres-secret --from-literal user=admin --from-literal password=asdfasdf`.
 
-And then fetch them and decode them using `kubectl get secret my-pg-password -o yaml | grep password | sed -n '1p' | awk '{print $2}' | base64 --decode`
+And then fetch them and decode them using `kubectl get secret my-postgres-secret -o yaml | grep password | sed -n '1p' | awk '{print $2}' | base64 --decode`.
--- seed-db.sh
+++ seed-db.sh
@@ -1,16 +1,42 @@
 #!/bin/bash -xe
 
-RUNNING_DB_SHA="$(docker ps | grep my_postgres | awk '{print $1}')"
+ENV=$1
 
-if [ -z "$RUNNING_DB_SHA" ]; then
-  echo "No running DB called my_postgres"
+if [ -z "$ENV" ]; then
+  echo "Missing first argument ENV. It should be either docker or k8s."
   exit 0
 fi
 
-DB_HOST=localhost
-DB_PORT=5600
-DB_USER=pg-user
-DB_PASSWORD=my-pg-password
-DB_NAME=my_postgres_db
+if [ "$ENV" = "docker" ]; then
+  RUNNING_DB_SHA="$(docker ps | grep my_postgres | awk '{print $1}')"
 
-psql postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME} < ./my-node-bootstrap/db/scripts/add_test_data.sql
+  if [ -z "$RUNNING_DB_SHA" ]; then
+    echo "No running DB called my_postgres"
+    exit 0
+  fi
+
+  DB_HOST=localhost
+  DB_PORT=5600
+  DB_USER=pg-user
+  DB_PASSWORD=my-pg-password
+  DB_NAME=my_postgres_db
+
+  psql postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME} < ./my-node-bootstrap/db/scripts/add_test_data.sql
+fi
+
+if [ "$ENV" = "k8s" ]; then
+  RUNNING_DB_PORT="$(kubectl get svc my-postgres-svc -o yaml | grep nodePort | awk '{print $3}')"
+
+  if [ -z "$RUNNING_DB_PORT" ]; then
+    echo "No running service called my-postgres-svc"
+    exit 0
+  fi
+
+  DB_HOST="$(minikube ip)"
+  DB_PORT="${RUNNING_DB_PORT}"
+  DB_USER=pg-user
+  DB_PASSWORD="$(kubectl get secret my-postgres-secret -o yaml | grep password | sed -n '1p' | awk '{print $2}' | base64 --decode)"
+  DB_NAME=my_postgres_db
+
+  psql postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME} < ./my-node-bootstrap/db/scripts/add_test_data.sql
+fi
